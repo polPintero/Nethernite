bb721b9cd5ae2ed273a137591afdcc3166ec9505
Dmitry <dmitry.tarabanov@decimadigital.com>
2021-07-17 12:32:48 +0300

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app id="app">
+    <v-app-bar color="blue" dense :height="50">
+
+    </v-app-bar>
+    <v-main color="blue"> </v-main>
+    <v-footer color="red"> </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
